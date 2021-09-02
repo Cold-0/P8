9a321424f31ea055d6a63f35fca13014d4fafb2a
Cold <coldragon78@gmail.com>
2021-09-02 02:59:36 +0200

    Fixed new Activity

--- app/src/main/java/com/cold0/realestatemanager/model/Photo.kt
+++ app/src/main/java/com/cold0/realestatemanager/model/Photo.kt
@@ -4,30 +4,30 @@ import androidx.room.Entity
 
 @Entity
 data class Photo(
-    val name: String = "",
-    val url: String = "",
-    var data: ByteArray? = null
+	val name: String = "",
+	val url: String = "",
+	var data: ByteArray? = null,
 ) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (javaClass != other?.javaClass) return false
 
-        other as Photo
+		other as Photo
 
-        if (name != other.name) return false
-        if (url != other.url) return false
-        if (data != null) {
-            if (other.data == null) return false
-            if (!data.contentEquals(other.data)) return false
-        } else if (other.data != null) return false
+		if (name != other.name) return false
+		if (url != other.url) return false
+		if (data != null) {
+			if (other.data == null) return false
+			if (!data.contentEquals(other.data)) return false
+		} else if (other.data != null) return false
 
-        return true
-    }
+		return true
+	}
 
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + url.hashCode()
-        result = 31 * result + (data?.contentHashCode() ?: 0)
-        return result
-    }
+	override fun hashCode(): Int {
+		var result = name.hashCode()
+		result = 31 * result + url.hashCode()
+		result = 31 * result + (data?.contentHashCode() ?: 0)
+		return result
+	}
 }
