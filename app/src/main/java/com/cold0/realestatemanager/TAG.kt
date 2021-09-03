8467dc2e242ac8b7e64c27ad9fe44cc637b50303
Cold <coldragon78@gmail.com>
2021-09-03 05:15:53 +0200

    Remove Tag, almost useless as Composable aren't inside class

--- app/src/main/java/com/cold0/realestatemanager/TAG.kt
+++
@@ -1,8 +0,0 @@
-package com.cold0.realestatemanager
-
-@Suppress("unused")
-val Any.TAG: String
-	get() {
-		val tag = javaClass.simpleName
-		return if (tag.length <= 23) tag else tag.substring(0, 23)
-	}
