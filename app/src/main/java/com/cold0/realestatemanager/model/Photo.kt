c8ca12d03ead73d1deaee88eaa92d915b4f4a5d3
Cold <coldragon78@gmail.com>
2021-09-06 05:00:08 +0200

    Remove useless Entity from Photo

--- app/src/main/java/com/cold0/realestatemanager/model/Photo.kt
+++ app/src/main/java/com/cold0/realestatemanager/model/Photo.kt
@@ -1,11 +1,9 @@
 package com.cold0.realestatemanager.model
 
 import android.os.Parcelable
-import androidx.room.Entity
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity
 data class Photo(
 	var name: String = "",
 	var description: String = "",
